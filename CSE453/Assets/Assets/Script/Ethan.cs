c3e1c1d9c2f450b4bfe64a045b6fc5e4ce9b6fbc
ledongyang <ledongya@buffalo.edu>
2016-03-22 20:14:03 -0400

    Unity soccer project
    Update shooting soccer ball, pick up soccer ball, walking animation,
    running animation (hold left shift), jumping animation (press space
    bar), press “r” to reset the soccer ball position.

---
+++ CSE453/Assets/Assets/Script/Ethan.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ethan : MonoBehaviour {
+
+	public Animator anim;
+	private float inputHori;
+	private float inputVerti;
+	private bool run;
+	private bool jump;
+
+	void Start () {
+		anim = GetComponent <Animator> ();
+		run = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputHori = Input.GetAxis ("Horizontal");
+		inputVerti = Input.GetAxis ("Vertical");
+		anim.SetFloat ("inputH", inputHori);
+		anim.SetFloat ("inputV", inputVerti);
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			run = true;
+		} else {
+			run = false;
+		}
+		anim.SetBool ("run", run);
+
+		if (Input.GetKey ("space")) {
+			jump = true;
+		} else {
+			jump = false;
+		}
+		anim.SetBool ("jump", jump);
+	}
+}
---
+++ CSE453/Assets/Assets/Script/ScoccerShooter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoccerShooter : MonoBehaviour {
+
+	GameObject soccer;
+	GameObject soccerHolder;
+	GameObject player;
+	Rigidbody rb;
+	public float SoccerSpeed=30;
+	public bool hasBall=true;
+	// Use this for initialization
+	void Start () {
+		soccerHolder = transform.Find ("soccerHolder").gameObject;
+		soccer = soccerHolder.transform.Find ("soccer").gameObject;
+		rb = soccer.GetComponent<Rigidbody> ();
+		player = this.gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && hasBall) {
+			hasBall = false;
+			soccer.transform.parent = null;
+			rb.isKinematic = false;
+			rb.velocity = Camera.main.transform.forward * SoccerSpeed;
+		}
+		if (Input.GetKeyDown ("r") && hasBall == false) {
+			ResetSoccer ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "soccerChild" && hasBall == false) {
+			Debug.Log ("pick up");
+			ResetSoccer ();
+		}
+	}
+
+	void ResetSoccer(){
+		hasBall = true;
+		rb.isKinematic = true;
+		soccer.transform.parent = soccerHolder.transform;
+		soccer.transform.localPosition = new Vector3 (0, 0, 0);
+	}
+}
