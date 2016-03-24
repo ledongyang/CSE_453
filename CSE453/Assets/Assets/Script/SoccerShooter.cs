424a9b6c55bdc47f18e3577d042908d0de9951e2
ledongyang <ledongya@buffalo.edu>
2016-03-24 14:44:42 -0400

    update shooting power bar
    update  shooting power bar where indicates the shooting power of Ethan.
    Right now player can adjust the shooting power by pressing "I" or "o"
    keys, eventually the shooting power will determined by IMU data where
    how fast the player swing his/her foot. The shooting power will reflect
    the ball's velocity in the game, the greater the shooting power, the
    faster the soccer travels.

---
+++ CSE453/Assets/Assets/Script/SoccerShooter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoccerShooter : MonoBehaviour {
+
+	GameObject soccer;
+	GameObject soccerHolder;
+	GameObject player;
+	Rigidbody rb;
+	public float SoccerSpeed=30f;
+	private float soccerMaxSpeed = 50f;
+	public bool hasBall=true;
+	public float fillAmount;
+	// Use this for initialization
+	void Start () {
+		soccerHolder = transform.Find ("soccerHolder").gameObject;
+		soccer = soccerHolder.transform.Find ("soccer").gameObject;
+		rb = soccer.GetComponent<Rigidbody> ();
+		player = this.gameObject;
+		fillAmount = SoccerSpeed / soccerMaxSpeed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && hasBall) {
+			hasBall = false;
+			soccer.transform.parent = null;
+			rb.isKinematic = false;
+			rb.velocity = Camera.main.transform.forward * SoccerSpeed;
+		}
+		if (Input.GetKeyDown ("r") && hasBall == false) {
+			ResetSoccer ();
+		}
+		if (Input.GetKey ("i") && SoccerSpeed < 50f) {
+			SoccerSpeed += 0.1f;
+		}
+		if (Input.GetKey ("o") && SoccerSpeed > 0.5f) {
+			SoccerSpeed -= 0.1f;
+		}
+		fillAmount = SoccerSpeed / soccerMaxSpeed;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "soccerChild" && hasBall == false) {
+			Debug.Log ("pick up");
+			ResetSoccer ();
+		}
+	}
+
+	void ResetSoccer(){
+		hasBall = true;
+		rb.isKinematic = true;
+		soccer.transform.parent = soccerHolder.transform;
+		soccer.transform.localPosition = new Vector3 (0, 0, 0);
+	}
+
+}
